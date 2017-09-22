35d01ab5a5e030b04e6cd8bc96ddac1643b599c5
Logan McNiece <logan@sproutsocial.com>
2017-09-22 12:53:29 -0500

    adds config variables

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,9 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+import ENV from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-    host: 'http://localhost:3000',
-    // host: 'http://vice-server.herokuapp.com'
+    host: ENV.APP.API_HOST,
     namespace: 'api',
     pathForType: function(type) {
       return Ember.String.pluralize(type).dasherize(type);
--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.API_HOST = 'https://localhost:3000'
   }
 
   if (environment === 'test') {
@@ -43,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.APP.API_HOST = 'https://vice-server.herokuapp.com'
   }
 
   return ENV;
